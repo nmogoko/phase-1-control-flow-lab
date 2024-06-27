8857fcc7a26a5cba922475996f3cbeb16365f279
nmogoko <+>
2024-06-27 17:18:28 +0100

    half complete lab work

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
-}
+ if (someValue <= 400)
+   {return ("This one is on me!")}
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someValue >=400 && someValue <=2000)
+   {return ("That will be twenty bucks.")}
+
+ else if(someValue >=2000 && someValue <=2500)
+    {return ("I will gladly take your thirty bucks.")}
+
+else if (someValue >2500)
+      {return("No can do.")}
 }
+ scuberGreetingForFeet((199))
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+//  city = ternaryCheckCity ? "Ok, sounds good.":
+}
+// ternaryCheckCity(("NYC"))
+
+// ternaryCheckCity(("Pittsburgh"))
 
 function switchOnCharmFromTip(){
   // Write your code here!
+
+
 }
